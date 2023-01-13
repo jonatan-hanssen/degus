0a703aa2d7549891965c76e4d152fa8ab3ab4e2b
jonatan-hanssen <jonatan.hanssen@outlook.com>
2023-01-13 12:44:26 +0000

    Make price deterministic so that all pages show the same

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-var orignalPrice = 0.0000512;
-var price = 0.0000512;
-
-var increase = 0;
-
 var pricecounter = document.getElementById("price");
-pricecounter.innerText = "" + price.toFixed(9) + " USD (0.000%)";
 
 function calculatePrice() {
-    price += 0.0000001*(Math.random() - 0.6);
-	increase = 100*price/orignalPrice - 100
-    pricecounter.innerText = "" + price.toFixed(9) + " USD (" + increase.toFixed(3) + "%)";
+	var today = new Date();
+	var x = today.getHours() * 3600 + today.getMinutes() * 60 + today.getSeconds();
+	var price = 1 - 0.01 * Math.sin(0.01*x) - 0.00001*x + 0.001*Math.sin(x);
+	price *= 5;
+	var increase = 100*price/5 - 100
+    pricecounter.innerText = "" + (price/100000).toFixed(9) + " USD (" + increase.toFixed(3) + "%)";
 }
 
-var cancel = setInterval(calculatePrice, 1500);
+var cancel = setInterval(calculatePrice, 1000);
+calculatePrice();
