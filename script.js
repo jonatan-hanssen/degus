4e9940c9ee4a5ebc02835b0c9750b9b5e0586905
jonatan-hanssen <jonatan.hanssen@outlook.com>
2023-01-13 10:19:21 +0000

    Add more files

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+var orignalPrice = 0.0000512;
 var price = 0.0000512;
+
+var increase = 0;
+
 var pricecounter = document.getElementById("price");
-pricecounter.innerText = "" + price.toFixed(9) + " USD";
+pricecounter.innerText = "" + price.toFixed(9) + " USD (0.000%)";
 
 function calculatePrice() {
     price += 0.0000001*(Math.random() - 0.6);
-    pricecounter.innerText = "" + price.toFixed(9) + " USD";
+	increase = 100*price/orignalPrice - 100
+    pricecounter.innerText = "" + price.toFixed(9) + " USD (" + increase.toFixed(3) + "%)";
 }
 
 var cancel = setInterval(calculatePrice, 1500);
